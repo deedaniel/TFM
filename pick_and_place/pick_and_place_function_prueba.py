11fdd6a59f546c2305825ec490f6f344a25a7ba5
deedaniel <700755@unizar.es>
2020-11-11 19:21:07 +0100

    Encuadre de las escenas.

--- pick_and_place/pick_and_place_function_prueba.py
+++ pick_and_place/pick_and_place_function_prueba.py
@@ -4,15 +4,15 @@ import numpy as np
 
 TASK_NAME = "pick_and_place"
 # coords_type = "esfericas"
-VARIATION = '2container'
+VARIATION = '1container'
 
-file = "solucion_bayesopt_" + TASK_NAME + "_" + VARIATION + ".p"
-wp_params = pickle.load(open(file, "rb"))
-print(wp_params[0])
+# file = "solucion_bayesopt_" + TASK_NAME + "_" + VARIATION + ".p"
+# wp_params = pickle.load(open(file, "rb"))
+# print(wp_params[0])
 
 function = fun.PickAndPlace(headless_mode=False, variation=VARIATION)  # Inicializacion
 
-pp_params = np.array([0.0, 0.0, -0.22, 0.125, 0.0, -0.22])
+pp_params = np.array([0.0, 0.0, -0.22, 0.0, 0.0, -0.22])
 reward = function.pick_and_place(pp_params)
 print(reward)
 
