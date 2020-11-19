226d3ad86ad3f7d49d87f1a0b662b7644643810e
deedaniel <700755@unizar.es>
2020-11-19 18:48:46 +0100

    Prueba de las soluciones de pick and place.

--- pick_and_place/pick_and_place_function_prueba.py
+++ pick_and_place/pick_and_place_function_prueba.py
@@ -3,17 +3,15 @@ import pickle
 import numpy as np
 
 TASK_NAME = "pick_and_place"
-# coords_type = "esfericas"
-VARIATION = '1container'
+VARIATION = '2container'
 
-# file = "solucion_bayesopt_" + TASK_NAME + "_" + VARIATION + ".p"
-# wp_params = pickle.load(open(file, "rb"))
-# print(wp_params[0])
+file = "solucion_difevol_" + TASK_NAME + "_" + VARIATION + ".p"
+wp_params = pickle.load(open(file, "rb"))
 
 function = fun.PickAndPlace(headless_mode=False, variation=VARIATION)  # Inicializacion
 
 pp_params = np.array([-0.06991656, -0.00729467, -0.24195188, -0., -0.0056952,  -0.14584554])
-reward = function.pick_and_place(pp_params)
+reward = function.pick_and_place(wp_params[4].x)
 print(reward)
 
 function.shutdown()  # Apagado
