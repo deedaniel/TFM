2b5d7134e9680d5cb1a046ff16c2e5e0db94fbb3
deedaniel <700755@unizar.es>
2020-11-20 18:25:57 +0100

    Graficas definitivas para evitacion de un obstaculo.

--- print_graph.py
+++ print_graph.py
@@ -5,7 +5,7 @@ import scipy.stats as sp
 
 TASK_DIR = "avoid_obstacle/"
 TASK_NAME = "avoid_obstacle"
-VARIATION = "cartesianas"
+VARIATION = "esfericas"
 
 file1 = TASK_DIR + "listas_bayesopt_" + TASK_NAME + "_" + VARIATION + ".p"
 resultados1 = pickle.load(open(file1, "rb"))
@@ -42,10 +42,10 @@ for resultado in lista_de_resultados:
     n, it = range(res.shape[0]), range(res.shape[1])
     t_limits = sp.t.interval(0.95, n_experimentos) / np.sqrt(n_experimentos)
 
-    # res = res[0:300]
-    # res_mean = res_mean[0:300]
-    # res_std = res_std[0:300]
-    # it = it[0:300]
+    res = res[0:50]
+    res_mean = res_mean[0:50]
+    res_std = res_std[0:50]
+    it = it[0:50]
 
     plt.plot(it, res_mean, linewidth=2, label=None)
     plt.fill(np.concatenate([it, it[::-1]]),
