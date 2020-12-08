8c09338b7b17a70059a5d04682b6d37860586fa4
deedaniel <700755@unizar.es>
2020-12-08 21:14:20 +0100

    Modificacion del archivo de hacer graficas y el de prueba para poder guardar los parametros de las mejores recompensas y cargarlos en el archivo de prueba de la función. Videos de tres politicas diferentes de Pick and Place.

--- pick_and_place/pick_and_place_function_prueba.py
+++ pick_and_place/pick_and_place_function_prueba.py
@@ -8,10 +8,16 @@ VARIATION = '2container'
 file = "solucion_difevol_" + TASK_NAME + "_" + VARIATION + ".p"
 wp_params = pickle.load(open(file, "rb"))
 
+file2 = "params_solution_" + TASK_NAME + "_" + VARIATION + ".p"
+best_params = pickle.load(open(file2, "rb"))
+
+params = best_params[0][0, 300]
+
+
 function = fun.PickAndPlace(headless_mode=False, variation=VARIATION)  # Inicializacion
 
-pp_params = np.array([-0.06991656, -0.00729467, -0.24195188, -0., -0.0056952,  -0.14584554])
-reward = function.pick_and_place(wp_params[4].x)
+# pp_params = np.array([-0.06991656, -0.00729467, -0.24195188, -0., -0.0056952,  -0.14584554])
+reward = function.pick_and_place(params)
 print(reward)
 
 function.shutdown()  # Apagado
