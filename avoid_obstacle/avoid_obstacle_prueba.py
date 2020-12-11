074b7cbdcdddd4190abc4162905aab78087a3a05
deedaniel <700755@unizar.es>
2020-12-11 19:50:14 +0100

    Video evitacion obstaculo para la presentacion.

--- avoid_obstacle/avoid_obstacle_prueba.py
+++ avoid_obstacle/avoid_obstacle_prueba.py
@@ -3,14 +3,14 @@ import avoid_obstacle.avoid_obstacle_function as fun
 import math
 import pickle
 
-COORDS = "esfericas"
+COORDS = "cartesianas"
 TASK_NAME = "avoid_obstacle" + "_" + COORDS
 
 file = "solucion_difevol_" + TASK_NAME + ".p"
 params_solution = pickle.load(open(file, "rb"))
 
 # radius = [0.27, 0.32]
-wp_params = np.array([0.0,  0.2,  0.0, 0.0,  0.4, 0.0])
+wp_params = np.array([0.0,  0.25,  0.3, 0.0,  0.25, 0.0])
 # wp_params = np.array([0.3, math.pi / 4.0, math.pi / 2.0, 0.4, math.pi / 2.0, math.pi / 2.0])
 
 function = fun.AvoidObstacle(headless_mode=False)  # Inicializacion
